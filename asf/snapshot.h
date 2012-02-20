871e54f5e93e64609d8de8f0e670c25f64783675
erik <erik@8ab6d134-22f3-c048-99a0-4997e65440b1>
2012-02-20 19:02:15 +0000

    No longer needed
    git-svn-id: svn://svn.kaashoek.com/comskip/trunk@47 8ab6d134-22f3-c048-99a0-4997e65440b1

--- asf/snapshot.h
+++
@@ -1,8 +0,0 @@
-typedef struct snapshot_t {
-  char *p_data;  /* Data area */
-
-  int i_width;       /* In pixels */
-  int i_height;      /* In pixels */
-  int i_datasize;    /* In bytes */
-  mtime_t date;      /* Presentation time */
-} snapshot_t;
