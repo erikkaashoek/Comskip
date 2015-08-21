dcf715619f5c3cb91435d04fea636beaa94bb356
Aman Gupta <aman@tmm1.net>
2015-08-20 23:12:06 -0700

    fix compile warning

--- mpeg2dec.c
+++ mpeg2dec.c
@@ -1346,7 +1346,7 @@ static int    prev_strange_framenum = 0;
                 for (i=0; i<sd->size; i++) {
                   ccData[i+7] = sd->data[i];
                 }
-                dump_data(ccData, (int)ccDataLen);
+                dump_data((char *)ccData, (int)ccDataLen);
                 if (processCC) ProcessCCData();
                 if (output_srt) process_block(ccData, (int)ccDataLen);
             }
