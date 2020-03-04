e4c79fdd60146b724eec900a3ceb1e9c9230d139
Eric Lindvall <eric@5stops.com>
2020-03-04 11:26:17 -0800

    Fix cases where variables are initialized differently

--- comskip.c
+++ comskip.c
@@ -14131,6 +14131,7 @@ again:
         fps = t  * 1.00000000000001;
     InitComSkip();
     frame_count = 1;
+    pict_type = '?';
     while (fgets(line, sizeof(line), in_file) != NULL)
     {
         i = 0;
@@ -14280,8 +14281,6 @@ again:
     }
 
 
-    FindLogoThreshold();
-
     height = maxmaxY + minminY;
     videowidth = width = maxmaxX + minminX;
 
@@ -14296,6 +14295,7 @@ again:
     min_brightness_found = 255;
     for (i = 1; i < frame_count; i++)
     {
+        framenum_real = i;
 ccagain:
         if (dump_data_file && ccDataFrame == 0)
         {
@@ -14373,9 +14373,6 @@ ccagain:
             silenceHistogram[(frame[i].volume < 255 ? frame[i].volume : 255)]++;
         }
 
-
-
-        framenum_real = i;
 
         if (frame[i].maxX == 0)
         {
@@ -15354,6 +15351,15 @@ void ProcessCCData(void)
     unsigned char	packetCount;
 
     if (!initialized) return;
+
+    // Reset state on the first frame
+    if (framenum == 0) {
+        last_cc_type = NONE;
+        current_cc_type = NONE;
+        cc_on_screen = false;
+        cc_in_memory = false;
+    }
+
     if (verbose >= 12)
     {
         p = (unsigned char *)temp;
